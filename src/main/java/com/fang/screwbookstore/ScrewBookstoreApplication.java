21752f8ba3649c99111857ac9812f983d021dc05
方耀辉 <2944163240@qq.com>
2023-10-14 22:22:00 +0800

    fangyaohui

--- src/main/java/com/fang/screwbookstore/ScrewBookstoreApplication.java
+++ src/main/java/com/fang/screwbookstore/ScrewBookstoreApplication.java
@@ -8,7 +8,7 @@ public class ScrewBookstoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScrewBookstoreApplication.class, args);
-//        System.out.println("fagnyaohui");
+        System.out.println("fagnyaohui 第一次修改");
     }
 
 }
