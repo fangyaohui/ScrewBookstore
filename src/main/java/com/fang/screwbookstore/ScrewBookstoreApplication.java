e40b96d14d5523480b6fdd103e1358f54d7f7e0f
方耀辉 <2944163240@qq.com>
2023-10-14 21:56:59 +0800

    fang

---
+++ src/main/java/com/fang/screwbookstore/ScrewBookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.fang.screwbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewBookstoreApplication.class, args);
+        System.out.println("fagnyaohui");
+    }
+
+}
