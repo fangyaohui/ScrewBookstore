397a4a73d1b95ed5406a2a4ead6de65e9e486442
方耀辉 <2944163240@qq.com>
2023-10-18 17:46:34 +0800

    fangyaohui

--- src/main/java/com/fang/screwbookstore/ScrewBookstoreApplication.java
+++ src/main/java/com/fang/screwbookstore/ScrewBookstoreApplication.java
@@ -2,13 +2,16 @@ package com.fang.screwbookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ServletComponentScan
 public class ScrewBookstoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScrewBookstoreApplication.class, args);
-        System.out.println("fagnyaohui 第二次修改");
+//        System.out.println("fagnyaohui 第二次修改");
     }
 
 }
